5b57e10b1b67da8f432af258346224ac0240aeb1
Shaz Momin <shazmomin2012@gmail.com>
2020-05-03 22:30:31 -0500

    Updated date.js & fixed padding on section

---
+++ scripts/date.js
@@ -0,0 +1,24 @@
+console.log("hello");
+
+function setDate() {
+    var d = Date(Date.now());
+    a = d.toString();
+
+    console.log(a);
+
+    var date = new Date();
+
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    document.getElementsByClassName("date")[0].innerHTML = days[date.getDay()] + ", " + months[date.getMonth()] + " " + date.getDate() + " " + date.getFullYear();
+
+    //console.log(days[date.getDay()] + ", " + months[date.getMonth()] + " " + date.getDate() + " " + date.getFullYear());
+}
+
+setDate();
+
+// Refreshes every 1000ms 
+setInterval(function(){
+    setDate();
+}, 1000);
