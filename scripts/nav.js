6da7f71efff3e485ec7e2ec8f70d9e736a4726f1
Shaz Momin <shazmomin2012@gmail.com>
2020-05-06 15:32:27 -0500

    Made nav.js more concise

--- scripts/nav.js
+++ scripts/nav.js
@@ -5,32 +5,15 @@ function openPage(pageName, elmnt) {
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-
+    
     // Show the specific tab content
     document.getElementById(pageName).style.display = "flex";
 
     // Resetting nav tab backgroundcolor/color values
-    document.getElementsByTagName("svg")[0].style.backgroundColor = "var(--bg-primary)";
-    document.getElementsByTagName("svg")[0].style.color = "var(--text-primary)";
-
-    document.getElementsByTagName("svg")[1].style.backgroundColor = "var(--bg-primary)";
-    document.getElementsByTagName("svg")[1].style.color = "var(--text-primary)";
-
-    document.getElementsByTagName("svg")[2].style.backgroundColor = "var(--bg-primary)";
-    document.getElementsByTagName("svg")[2].style.color = "var(--text-primary)";
-
-    document.getElementsByTagName("svg")[3].style.backgroundColor = "var(--bg-primary)";
-    document.getElementsByTagName("svg")[3].style.color = "var(--text-primary)";
-
-    document.getElementsByTagName("svg")[4].style.backgroundColor = "var(--bg-primary)";
-    document.getElementsByTagName("svg")[4].style.color = "var(--text-primary)";
-
-    document.getElementsByTagName("svg")[5].style.backgroundColor = "var(--bg-primary)";
-    document.getElementsByTagName("svg")[5].style.color = "var(--text-primary)";
-
-    document.getElementsByTagName("svg")[6].style.backgroundColor = "var(--bg-primary)";
-    document.getElementsByTagName("svg")[6].style.color = "var(--text-primary)";
-
+    for (j = 0; j < 7; j++) {
+        document.getElementsByTagName("svg")[j].style.backgroundColor = "var(--bg-primary)";
+        document.getElementsByTagName("svg")[j].style.color = "var(--text-primary)";
+    }
 
     // Make the nav tab active
     elmnt.firstElementChild.style.backgroundColor = "var(--bg-secondary)";
