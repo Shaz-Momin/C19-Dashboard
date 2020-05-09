c496525963f5ca4e767d15ef890a2bd530d5784d
Shaz Momin <shazmomin2012@gmail.com>
2020-05-08 19:35:45 -0500

    Made US by States panel content functional

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,14 @@
 console.log("Hello coder");
 var total = "";
-var table = "";
+var tableWW = "";
+var tableStates = "";
 
-var url = "https://covid19-server.chrismichael.now.sh/api/v1/AllReports";
+var urlWW = "https://covid19-server.chrismichael.now.sh/api/v1/AllReports";
+var urlStates = "https://covid19-server.chrismichael.now.sh/api/v1/UnitedStateCasesByStates";
 updateData();
 
 function updateData() {
-    $.getJSON(url,(dat) => {
+    $.getJSON(urlWW,(dat) => {
         fetch(dat);
     
         //console.log(getWW());
@@ -20,17 +22,49 @@ function updateData() {
             $(".g-new-deaths").html(getWW().NewDeaths); 
             $(".g-active-cases").html(getWW().ActiveCases); 
         }
+
         
         function fetch(data){
             total = data.reports[0];
-            table = data.reports[0].table;
+            tableWW = data.reports[0].table;
         }
     
         function getWW() {
-            return ("WorldWide: ", table[0][0]);
+            return ("WorldWide: ", tableWW[0][0]);
         }
     });
+
+    $.getJSON(urlStates, (dat) => {
+        fetch(dat);
+        
+        //console.log(tableStates);
+
+        $("#US-states").change(function() {
+            setStateContent($("#US-states").val());
+            
+        })
+
+        function setStateContent(val) {
+            for (var i = 0; i < tableStates.length; i++) {
+                if (tableStates[i].state == val) {
+                    $(".s-total-test-results").html((tableStates[i].totalTestResults).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+                    $(".s-positive").html((tableStates[i].positive).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+                    $(".s-negative").html((tableStates[i].negative).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+                    $(".s-total-deaths").html((tableStates[i].death).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+                    $(".s-data-quality-grade").html((tableStates[i].dataQualityGrade).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+                }
+            }
+        }
+    
+    
+        function fetch(database) {
+            tableStates = database.data[0].table;
+        }
+    
+        
+    });
 }
+
 
 // Refreshes every 5000ms
 setInterval(function(){
