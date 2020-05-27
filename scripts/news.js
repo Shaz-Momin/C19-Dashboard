882e21d7291fa4e7b3d4e240f4946350b6044f64
Shaz Momin <shazmomin2012@gmail.com>
2020-05-26 23:15:00 -0500

    Major changes to news.js & articles (new src: NYT)

--- scripts/news.js
+++ scripts/news.js
@@ -1,40 +1,92 @@
-let sourceUrl = "https://newsapi.org/v2/top-headlines?sources=";
-let src = "";
-let apiKey = "&language=en&apiKey=c3ebca5046794ae2b2087639cde8fea0";
+let sourceUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+let query = "&q=coronavirus";
+let sort = "&sort=newest";
+let apiKey = "&api-key=tsAmKyrp4kA9AAQPgzDt3K5y0zHSi7nM";
+let rootImgSrc = "https://www.nytimes.com/";
 var date = new Date();
 
-let srcList = ["cnn", "cbs-news","cnbc","business-insider","bbc-news"];
 
-//localStorage.clear();
-let data = [{}];
 
-if (window.localStorage.length != 1) {
-    console.log("Adding news data");
-    getNews();
-} else {
-    console.log(JSON.parse(window.localStorage.getItem("Data")));
+$.getJSON("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&sort=newest&type_of_material=news&api-key=tsAmKyrp4kA9AAQPgzDt3K5y0zHSi7nM", (data) => {
+    //console.log(data);
+}) 
+
+updateTopLatest();
+
+// Refreshes every 30 seconds
+setInterval(() => {
+    updateTopLatest();
+}, 30000);
+
+
+function updateTopLatest() {
+    var node= document.getElementById("latest-top");
+    node.querySelectorAll('*').forEach(n => n.remove());
+
+
+    $.getJSON(sourceUrl + query + sort + apiKey, (dat) => {
+        setArticles();
+
+        function setArticles() {
+            //var LatestTop = JSON.parse(window.localStorage.getItem("Latest-Top"));
+            //console.log(LatestTop);
+            var LatestArr = dat.response.docs;
+            for (var i = 0; i < LatestArr.length; i++) {
+                var elm = document.getElementsByClassName("gridContainer")[0];
+                var newElm = elm.cloneNode(true);
+                
+                newElm.style.setProperty("display","grid");
+                // Setting new attributes to article widget
+                if (LatestArr[i].multimedia.length != 0) {
+                    newElm.getElementsByTagName("img")[0].src = rootImgSrc + LatestArr[i].multimedia[10].url;
+                    newElm.setAttribute("class", "gridContainer imgInc");
+                } else {
+                    newElm.getElementsByTagName("img")[0].style.display = "none";
+                    newElm.style.setProperty("grid-column-end", "span 1");
+                }
+
+                newElm.getElementsByTagName("a")[0].innerHTML = LatestArr[i].headline.main;
+                newElm.getElementsByTagName("a")[0].href = LatestArr[i].web_url;
+                newElm.getElementsByClassName("des")[0].innerHTML = LatestArr[i].snippet;
+                newElm.getElementsByClassName("datePublished")[0].innerHTML = formatPubAt(LatestArr[i].pub_date);
+
+                // Adding the new widget {node}
+                document.getElementById("latest-top").appendChild(newElm);
+            }
+        }
+
+        function formatPubAt(str) {
+            var strArr = str.split("T");
+            var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+            
+            var month = months[parseInt(strArr[0].substring(5,7)) - 1];
+            var numDate = strArr[0].substring(8);
+        
+            var hr = strArr[1].substring(0,2);
+            var min = strArr[1].substring(3,5);
+        
+            var timeState = "";
+            if (parseInt(hr) > 12) {
+                hr = hr - 12;
+                timeState = " P.M.";
+            } else if (parseInt(hr) == 12) {
+                timeState = " P.M.";
+            } else {
+                timeState = " A.M.";
+            }
+            //console.log(month + " " + numDate + " | " + hr + ":" + min + timeState);
+            return (month + " " + numDate + " | " + hr + ":" + min + timeState);
+        }
+        
+
+    })
 }
 
-if (date.getHours() == 23 && date.getMinutes() == 59 && date.getSeconds() == 59) {
-    console.log("Updating news");
-    updateNews();
-}
 
-function getNews() {
-    for (var i = 0; i < srcList.length; i++) {
-        $.getJSON(sourceUrl + srcList[i] + apiKey, (dat) => {
-            data.push({dat});
-            //localStorage.setItem(srcList[i], JSON.stringify(dat));
-            //console.log(dat);
-            window.localStorage.setItem("Data", JSON.stringify(data));
-        })
-    }
-}
 
-function updateNews() {
-    window.localStorage.clear();
-    getNews();
-}
+
+
+
 
 
 //localStorage.setItem("Data", data);
@@ -45,3 +97,34 @@ function updateNews() {
     "number": 1,
     "gameMode": true
 })) */
+
+  
+    /* var x = new XMLHttpRequest();
+    x.open('GET', 'https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=797d8857b43b40938df73b9155aa59a2');
+    // I put "XMLHttpRequest" here, but you can use anything you want.
+    x.setRequestHeader('X-Requested-With', 'XMLHttpRequest', 'Origin');
+    x.onload = function() {
+        alert(x.responseText);
+    };
+    x.send(); */
+    /* $.ajax({
+        url: 'https://newsapi.org/v2/top-headlines?sources=bbc-news&callback=?&apiKey=797d8857b43b40938df73b9155aa59a2', 
+        type: 'GET',
+        headers: {'Access-Control-Allow-Origin':'*','Origin':'X-Requested-With'},
+        dataType: 'json', 
+        success: function(data){
+            data.push({"dat": data});
+            console.log(data);
+        } 
+    });*/
+    /* fetch("https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=c3ebca5046794ae2b2087639cde8fea0", {
+        headers: {
+            'Content-Type': 'application/json',
+            'Origin':'X-requested-with'
+        }
+    }).then((response) => {
+        return response.text();
+    }).then((art) => {
+        console.log(art);
+    }) */
+    //getNews();
